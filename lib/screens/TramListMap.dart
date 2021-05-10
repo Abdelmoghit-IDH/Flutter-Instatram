ffa668ef74e614a90ef68387616f227703d52dcb
abdelmoghit idhsaine <idhsaine.abdelmoghit@inemail.ine.inpt.ma>
2021-05-10 15:31:57 +0000

    commit

--- lib/screens/TramListMap.dart
+++ lib/screens/TramListMap.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
-
 import '../models/tram.dart';
 import '../service/tram_service.dart';
 
